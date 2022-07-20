32bf104fa4d8974e076537d77fa47d8956e5fc51
zhen <2440913157@qq.com>
2022-07-20 17:24:09 +0800

    push commit

--- src/main/java/com/zhen/git/GitTest.java
+++ src/main/java/com/zhen/git/GitTest.java
@@ -11,6 +11,7 @@ public class GitTest {
         System.out.println("hello git3");
         System.out.println("master");
         System.out.println("hot-fix");
+        System.out.println("push");
 
     }
 }
