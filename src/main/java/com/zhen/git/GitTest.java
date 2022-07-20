239c001dd955e94a744ce4d9272a7254616eac16
zhenzihan <89240075+zhenzihan-polaris@users.noreply.github.com>
2022-07-20 17:27:56 +0800

    pull commit

--- src/main/java/com/zhen/git/GitTest.java
+++ src/main/java/com/zhen/git/GitTest.java
@@ -12,6 +12,7 @@ public class GitTest {
         System.out.println("master");
         System.out.println("hot-fix");
         System.out.println("push");
+        System.out.println("pull");
 
     }
 }
